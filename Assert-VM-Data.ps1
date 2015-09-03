92206930110eaf8e99898f3a7f26bd9e979e0c01
Nana Lakshmanan <nanalakshmanan@gmail.com>
2015-09-02 18:32:30 -0700

    Get repos, save resource kit modules locally,setup VMs

---
+++ Assert-VM-Data.ps1
@@ -0,0 +1,98 @@
+﻿$ScriptPath = Split-Path $MyInvocation.MyCommand.Path
+$WorkingFolder = 'D:\Nana\Test'
+$ContentFolder = 'D:\Nana\Content\'
+$Credential = Get-Credential Administrator
+
+@{
+    AllNodes = @(
+        @{
+            NodeName = 'localhost';
+            Role     = 'HyperVHost'      # HyperVHost as the role identifies
+                                         # every Hyper-V host node for which
+                                         # this configuration will be compiled
+
+            # One switch can be created overall
+            SwitchName        = 'DemoSwitchInternal'
+            SwitchType        = 'Internal'
+            SwitchIPv4Address = '192.168.1.10'
+
+            # path where diff vhds will be created
+            VhdPath         = "$WorkingFolder\Vhd"
+
+            # path where VM data will be stored
+            VMPath          = "$WorkingFolder\VM"
+
+            # VMType is an array of hashtables
+            # each entry contains data for VMs created from a single
+            # vhd source
+
+            VMType = @(
+              <#@{
+                # name of the VMType
+                Name            = 'TestVM'
+
+                # location for the source vhd
+                VhdSource       = 'D:\VHD\Golden\Nana-Test.vhd'
+
+                # VMName is an array and will be combined with namebase to 
+                # create VM names like Nana-Test-DC, Nana-Test-WS, etc
+
+                VMNameBase        = 'Nana-Test'
+                VMName            = @('1')
+                VMIPAddress       = @('192.168.1.1')
+                VMStartupMemory   = 4GB
+                VMState           = 'Running'
+                VMUnattendPath    = "$ScriptPath\unattend.xml"
+                VMUnattendCommand = "$ScriptPath\unattend.cmd"
+
+                # Administrator credentials
+                VMAdministratorCredentials = $Credential
+
+                # This is the modules folder. Everything under this folder
+                # will be copied to $Env:ProgramFiles\WindowsPowerShell\Modules
+                VMModulesFolder = "$ContentFolder\Modules"
+
+                #The folders to inject into this vhd. These will be
+                #available under \content
+                VMFoldersToCopy = @(
+                                        $ContentFolder
+                                    )
+
+              },#>
+
+              @{
+                # name of the VMType
+                Name            = 'XM-TestVM'
+
+                # location for the source vhd
+                VhdSource       = 'D:\Nana\Test\Vhd\Base\Nana-XM.vhd'
+
+                # VMName is an array and will be combined with namebase to 
+                # create VM names like Nana-Test-DC, Nana-Test-WS, etc
+
+                VMNameBase        = 'Nana-XM'
+                VMName            = @('DC', 'Node')
+                VMIPAddress       = @('192.168.1.10', '192.168.1.11')
+                VMStartupMemory   = 4GB
+                VMState           = 'Running'
+                VMUnattendPath    = "$ScriptPath\unattend.xml"
+                VMUnattendCommand = "$ScriptPath\unattend.cmd"
+
+                # Administrator credentials
+                VMAdministratorCredentials = $Credential
+
+                # This is the modules folder. Everything under this folder
+                # will be copied to $Env:ProgramFiles\WindowsPowerShell\Modules
+                VMModulesFolder = (Join-Path $ContentFolder 'Modules')
+
+                #The folders to inject into this vhd. These will be
+                #available under \content
+                VMFoldersToCopy = @(
+                                        $ContentFolder
+                                    )
+
+              }
+            )
+        }
+    )
+}
---
+++ Get-Repo.ps1
@@ -0,0 +1,101 @@
+﻿[CmdletBinding(SupportsShouldProcess=$true)]
+param()
+
+function Ensure-Folder
+{
+    [CmdletBinding()]
+
+    param(
+        [string]
+        $Path
+    )
+
+    if (-not (Test-Path $Path))
+    {
+        Write-Verbose "Creating $Path"
+        md $Path > $Null
+    }
+}
+
+function Get-Branch
+{
+    param(
+        [string]
+        $RepoFolder
+    )
+
+    $Branches = @()
+    try
+    {
+        pushd $RepoFolder
+        $results = (git branch)
+        
+        $results | % {
+            if ($_.StartsWith('*'))
+            {
+                $Branches += ($_.Substring(1)).Trim()
+            }
+            else
+            {
+                $Branches += $_.Trim()
+            }
+        }
+    }
+    finally
+    {
+        popd
+    }
+
+    return $Branches
+}
+
+if ($psISE)
+{
+    throw 'Run this script from a PowerShell console (not ISE)'
+}
+
+$Url = 'https://api.github.com/users/nanalakshmanan/repos'
+$BaseFolder = 'D:\Nana\Official\git'
+
+Write-Verbose "Getting list of repos from $Url"
+
+$Repos = Invoke-RestMethod -Uri $Url
+
+Ensure-Folder $BaseFolder
+
+try
+{
+    pushd $BaseFolder
+
+    $Repos | % {
+
+        $RepoFolder = Join-Path $BaseFolder $_.Name
+
+        if (Test-Path $RepoFolder)
+        {
+            if ($PSCmdlet.ShouldProcess($_.Name, "pull changes"))
+            {
+                Write-Verbose "$($_.Name) exists, pulling latest changes"
+                pushd $RepoFolder
+
+                Get-Branch -RepoFolder $RepoFolder | % {
+                    git pull origin $_ 2> $Null
+                }
+                popd
+            }
+        }
+        else
+        {
+            if ($PSCmdlet.ShouldProcess($_.Name, "clone"))
+            {
+                Write-Verbose "$($_.Name) does not exist, cloning"
+                git clone $_.clone_url
+            }
+        }
+    }
+}
+finally
+{
+    popd
+}
+
